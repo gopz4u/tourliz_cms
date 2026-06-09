657b9115a9317d4479d9e89ac844b3ae4e2a0e54
gopz4u <gopu.pk@gmail.com>
2026-06-09 08:08:13 +0800

    Update view_logs.php with memory-efficient tail implementation

--- public/view_logs.php
+++ public/view_logs.php
@@ -5,14 +5,27 @@ if (!file_exists($logFile)) {
     exit;
 }
 
-$lines = 500;
-$data = file($logFile);
-$lineCount = count($data);
-$start = max(0, $lineCount - $lines);
+$bytesToRead = 100000; // Read last 100KB of logs
+$size = filesize($logFile);
+$file = fopen($logFile, 'r');
 
-echo "<h3>Last $lines lines of laravel.log</h3>";
-echo "<pre>";
-for ($i = $start; $i < $lineCount; $i++) {
-    echo htmlspecialchars($data[$i]);
+if ($file) {
+    if ($size > $bytesToRead) {
+        fseek($file, -$bytesToRead, SEEK_END);
+    }
+    
+    // Discard the first partial line if we seeked
+    if ($size > $bytesToRead) {
+        fgets($file);
+    }
+
+    echo "<h3>Last " . round($bytesToRead / 1024) . " KB of laravel.log</h3>";
+    echo "<pre>";
+    while (!feof($file)) {
+        echo htmlspecialchars(fgets($file));
+    }
+    echo "</pre>";
+    fclose($file);
+} else {
+    echo "Failed to open log file.";
 }
-echo "</pre>";
