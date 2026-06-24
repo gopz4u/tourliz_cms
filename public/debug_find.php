b15bc420ea148f2646da42037cf746226de618e8
gopz4u <gopu.pk@gmail.com>
2026-06-24 19:33:26 +0800

    Add temporary db query debug script

---
+++ public/debug_find.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+$id = 16;
+$itinerary = \App\Models\B2CItinerary::withTrashed()->find($id);
+
+header('Content-Type: text/plain');
+
+if ($itinerary) {
+    echo "Found B2C: " . $itinerary->quote_id . "\n";
+    print_r($itinerary->toArray());
+} else {
+    echo "Not found B2C with ID $id. Listing all B2C itineraries:\n";
+    foreach (\App\Models\B2CItinerary::withTrashed()->get() as $b) {
+        echo $b->id . " - " . $b->quote_id . " (Deleted: " . ($b->deleted_at ? 'Yes' : 'No') . ")\n";
+    }
+}
